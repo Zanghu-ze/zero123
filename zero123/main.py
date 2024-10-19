09f68ad569befb6ddc9d038363f9cebfb71fbf06
Zanghu-ze <740101132@qq.com>
2024-10-19 09:29:57 +0800

    Change the log category

--- zero123/main.py
+++ zero123/main.py
@@ -738,7 +738,7 @@ if __name__ == "__main__":
                 }
             },
         }
-        default_logger_cfg = default_logger_cfgs["wandb"]
+        default_logger_cfg = default_logger_cfgs["testtube"]
         if "logger" in lightning_config:
             logger_cfg = lightning_config.logger
         else:
